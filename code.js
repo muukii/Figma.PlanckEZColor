d7a830e4fc35f2ca724cadd9ff84a0fc4725e083
Muukii <muukii.app@gmail.com>
2022-10-17 00:42:26 +0900

    :evergreen_tree: Update

--- code.js
+++ code.js
@@ -1,72 +1,63 @@
 figma.showUI(__html__);
 function RGBtoHSV(r, g, b) {
-  let v = Math.max(r, g, b),
-    c = v - Math.min(r, g, b);
-  let h =
-    c && (v == r ? (g - b) / c : v == g ? 2 + (b - r) / c : 4 + (r - g) / c);
-  return { h: 60 * (h < 0 ? h + 6 : h), s: v && c / v, v: v };
+    let v = Math.max(r, g, b), c = v - Math.min(r, g, b);
+    let h = c && (v == r ? (g - b) / c : v == g ? 2 + (b - r) / c : 4 + (r - g) / c);
+    return { h: 60 * (h < 0 ? h + 6 : h), s: v && c / v, v: v };
 }
 figma.ui.onmessage = (msg) => {
-  switch (msg.type) {
-    case "cancel":
-      figma.closePlugin();
-      break;
-    case "run":
-      console.log("hello");
-      let colors = [];
-
-      const rectangles = figma.currentPage.selection.filter((e) => {
-        return e.type === "RECTANGLE";
-      });
-
-      const debugNode = (node) => {
-        console.log(node.x, node.y);
-      };
-
-      const _sort = (collection) => {
-        collection.sort((a, b) => {
-          if (a.x < b.x) {
-            return -1;
-          } else {
-            return 1;
-          }
-        });
-      };
-
-      const line0 = rectangles.filter((e) => e.y == 0);
-      const line1 = rectangles.filter((e) => e.y == 1);
-      const line2 = rectangles.filter((e) => e.y == 2);
-      const line3 = rectangles.filter((e) => e.y == 3);
-
-      _sort(line0);
-      _sort(line1);
-      _sort(line2);
-      _sort(line3);
-
-      line0.forEach(debugNode);
-      line1.forEach(debugNode);
-      line2.forEach(debugNode);
-      line3.forEach(debugNode);
-
-      let sortedRectangles = [];
-      sortedRectangles = sortedRectangles.concat(line0, line1, line2, line3);
-
-      console.log(sortedRectangles);
-
-      for (const node of sortedRectangles) {
-        console.log(node);
-        //   console.log(node.x, node.y);
-
-        const color = node.fills[0].color;
-        const hsb = RGBtoHSV(color.r, color.g, color.b);
-        colors.push(
-          `{${Math.round((hsb.h / 360) * 255)}, ${Math.round(
-            hsb.s * 255
-          )}, ${Math.round(hsb.v * 255)}}`
-        );
-      }
-      const output = `{ ${colors.join(",")} }`;
-      console.log(output);
-      figma.ui.postMessage(output);
-  }
+    switch (msg.type) {
+        case "cancel":
+            figma.closePlugin();
+            break;
+        case "run":
+            console.log("hello");
+            let colors = [];
+            const frames = figma.currentPage.selection.filter((e) => {
+                return e.type === "FRAME";
+            });
+            if (frames.length == 0) {
+                return;
+            }
+            const frame = frames[0];
+            console.log(frame.children);
+            const rectangles = frame.children.filter((e) => {
+                return e.type === "RECTANGLE";
+            });
+            const debugNode = (node) => {
+                console.log(node.x, node.y);
+            };
+            const maxX = Math.max.apply(null, rectangles.map((e) => e.x));
+            const maxY = Math.max.apply(null, rectangles.map((e) => e.y));
+            const getColor = (x, y) => {
+                const _rectangles = rectangles.filter((e) => e.x == x && e.y == y);
+                if (_rectangles.length == 0) {
+                    return null;
+                }
+                const rectangle = _rectangles[0];
+                const color = rectangle.fills[0].color;
+                const hsb = RGBtoHSV(color.r, color.g, color.b);
+                return hsb;
+            };
+            const lines = [];
+            for (let y = 0; y <= maxY; y++) {
+                const line = [];
+                for (let x = 0; x <= maxX; x++) {
+                    const color = getColor(x, y);
+                    if (color) {
+                        console.log(x, y, color);
+                        line.push(color);
+                    }
+                }
+                lines.push(line);
+            }
+            const flatten = lines
+                .flatMap((e) => e)
+                .map((color) => {
+                return `{${Math.round((color.h / 360) * 255)}, ${Math.round(color.s * 255)}, ${Math.round(color.v * 255)}}`;
+            })
+                .join(",");
+            const output = `{ ${flatten} }`;
+            console.log(output);
+            figma.ui.postMessage(output);
+    }
 };
---
+++ code.ts
@@ -0,0 +1,95 @@
+figma.showUI(__html__);
+
+type HSB = {
+  h: number;
+  s: number;
+  v: number;
+};
+
+function RGBtoHSV(r, g, b): HSB {
+  let v = Math.max(r, g, b),
+    c = v - Math.min(r, g, b);
+  let h =
+    c && (v == r ? (g - b) / c : v == g ? 2 + (b - r) / c : 4 + (r - g) / c);
+  return { h: 60 * (h < 0 ? h + 6 : h), s: v && c / v, v: v };
+}
+
+figma.ui.onmessage = (msg) => {
+  switch (msg.type) {
+    case "cancel":
+      figma.closePlugin();
+      break;
+    case "run":
+      console.log("hello");
+      let colors = [];
+
+      const frames = figma.currentPage.selection.filter((e) => {
+        return e.type === "FRAME";
+      }) as FrameNode[];
+
+      if (frames.length == 0) {
+        return;
+      }
+
+      const frame = frames[0];
+
+      console.log(frame.children);
+
+      const rectangles = frame.children.filter((e) => {
+        return e.type === "RECTANGLE";
+      }) as RectangleNode[];
+
+      const debugNode = (node) => {
+        console.log(node.x, node.y);
+      };
+
+      const maxX = Math.max.apply(
+        null,
+        rectangles.map((e) => e.x)
+      );
+      const maxY = Math.max.apply(
+        null,
+        rectangles.map((e) => e.y)
+      );
+
+      const getColor = (x: number, y: number) => {
+        const _rectangles = rectangles.filter((e) => e.x == x && e.y == y);
+        if (_rectangles.length == 0) {
+          return null;
+        }
+        const rectangle = _rectangles[0];
+
+        const color = rectangle.fills[0].color;
+        const hsb = RGBtoHSV(color.r, color.g, color.b);
+        return hsb;
+      };
+
+      const lines: HSB[][] = [];
+      for (let y = 0; y <= maxY; y++) {
+        const line: HSB[] = [];
+
+        for (let x = 0; x <= maxX; x++) {
+          const color = getColor(x, y);
+          if (color) {
+            console.log(x, y, color);
+            line.push(color);
+          }
+        }
+
+        lines.push(line);
+      }
+
+      const flatten = lines
+        .flatMap((e) => e)
+        .map((color) => {
+          return `{${Math.round((color.h / 360) * 255)}, ${Math.round(
+            color.s * 255
+          )}, ${Math.round(color.v * 255)}}`;
+        })
+        .join(",");
+
+      const output = `{ ${flatten} }`;
+      console.log(output);
+      figma.ui.postMessage(output);
+  }
+};
